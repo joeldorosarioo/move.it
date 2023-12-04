bf8c60564e85ddbc42b3e961b0f8ea3937ac8383
Joel Oliveira <joeldorosarioo@gmail.com>
2023-12-04 10:57:03 -0300

    style: adding global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import clsx from 'clsx';
+import { Inter } from 'next/font/google'
+
+import '@stylesheets/globals.css'
 
 export const metadata: Metadata = {
 	title: 'Move.it - Pomodoro and Challenges: The Formula for an Active Lifestyle.',
 	description: 'Enhance your well-being with Move.it, a project integrating the Pomodoro technique with invigorating challenges for computer users.',
 }
 
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['400', '500', '600']
+});
+
 export default function RootLayout ({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className="bg-zinc-100 text-zinc-950">
+			<body className={ clsx('text-lg bg-zinc-100 text-zinc-950 h-screen', inter.className) }>
 				{ children }
 			</body>
 		</html>
